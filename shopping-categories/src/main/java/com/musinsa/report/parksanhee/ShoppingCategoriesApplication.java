8997d07758af0163a32735aa129e0a78b1d9d17e
sanhee <68936@naver.com>
2022-07-14 14:47:17 +0900

    feat: 스프링 부트 프로젝트 초기 설정

---
+++ shopping-categories/src/main/java/com/musinsa/report/parksanhee/ShoppingCategoriesApplication.java
@@ -0,0 +1,13 @@
+package com.musinsa.report.parksanhee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCategoriesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingCategoriesApplication.class, args);
+    }
+
+}
---
+++ shopping-categories/src/test/java/com/musinsa/report/parksanhee/ShoppingCategoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musinsa.report.parksanhee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCategoriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
